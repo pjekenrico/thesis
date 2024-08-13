2546f2269e677733bba379ae7732ddcdc7fcb2b9
pjekenrico <pablojeken@hotmail.com>
2024-08-14 01:53:56 +0200

    Latest version review

--- compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pdflatex -shell-escape Main.tex \
-&& \
-( for i in $(grep "^\\\include" Main.tex | sed -e 's/.*{\(.*\)}.*/\1/'); do bibtex $i; done; ) \
-&& pdflatex -shell-escape Main.tex \
-&& pdflatex -shell-escape Main.tex
--- runbib.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-for i in $(grep "^\\\include" Main.tex | sed -e 's/.*{\(.*\)}.*/\1/'); do bibtex $i; done;
